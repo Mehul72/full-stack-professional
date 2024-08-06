cf12d62f33555a05fe18c87fcfa1d9e5dc553738
Mehul <z5477442@ad.unsw.edu.au>
2024-08-06 20:29:59 +1000

    react - profile picture uploads

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -3,6 +3,8 @@ package com.amigoscode;
 import com.amigoscode.customer.Customer;
 import com.amigoscode.customer.CustomerRepository;
 import com.amigoscode.customer.Gender;
+import com.amigoscode.s3.S3Buckets;
+import com.amigoscode.s3.S3Service;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.io.IOException;
 import java.util.Random;
 import java.util.UUID;
 
@@ -26,23 +29,38 @@ public class Main {
             CustomerRepository customerRepository,
             PasswordEncoder passwordEncoder) {
         return args -> {
-            var faker = new Faker();
-            Random random = new Random();
-            Name name = faker.name();
-            String firstName = name.firstName();
-            String lastName = name.lastName();
-            int age = random.nextInt(16, 99);
-            Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
-            String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@amigoscode.com";
-            Customer customer = new Customer(
-                    firstName +  " " + lastName,
-                    email,
-                    passwordEncoder.encode("password"),
-                    age,
-                    gender);
-            customerRepository.save(customer);
-            System.out.println(email);
+            createRandomCustomer(customerRepository, passwordEncoder);
+            // testBucketUploadAndDownload(s3Service, s3Buckets);
         };
+    }
+
+    private static void testBucketUploadAndDownload(S3Service s3Service, S3Buckets s3Buckets) throws IOException {
+        s3Service.putObject(
+                s3Buckets.getCustomer(),
+                "foo",
+                "Hello World".getBytes());
+        byte[] obj = s3Service.getObject("fs-mehul-customer-test", "foo");
+
+        System.out.println("Horrayfwfw: " + new String(obj));
+    }
+
+    private static void createRandomCustomer(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        var faker = new Faker();
+        Random random = new Random();
+        Name name = faker.name();
+        String firstName = name.firstName();
+        String lastName = name.lastName();
+        int age = random.nextInt(16, 99);
+        Gender gender = age % 2 == 0 ? Gender.MALE : Gender.FEMALE;
+        String email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@amigoscode.com";
+        Customer customer = new Customer(
+                firstName +  " " + lastName,
+                email,
+                passwordEncoder.encode("password"),
+                age,
+                gender);
+        customerRepository.save(customer);
+        System.out.println(email);
     }
 
 }
